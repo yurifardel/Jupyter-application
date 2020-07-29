5dbd1afd8531ca9b530ca86a9303f1bbd370db86
yurifardel <yrnascimento@outlook.com>
2020-07-29 00:07:16 -0300

    install react router dom and react slick to carousel

--- src/components/Menu/components/ButtonLink/index.js
+++ src/components/Menu/components/ButtonLink/index.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-function ButtonLink({ className, href, children }) {
+function ButtonLink(props){
   return (
-    <a className={className} href={href}>
-      {children}
+    <a href={props.href} className={props.className}>
+      {props.children}
     </a>
-  );
+  )
 }
-
-ButtonLink.defaultProps = {
-  href: '/',
-  className: '',
-};
-
-ButtonLink.propTypes = {
-  className: PropTypes.string,
-  href: PropTypes.string,
-  children: PropTypes.node.isRequired,
-};
-
 export default ButtonLink;
