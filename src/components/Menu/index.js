01b14d73f5845752018f46e7f8dcb810f0e9ca65
yurifardel <yrnascimento@outlook.com>
2020-07-29 04:03:51 -0300

    Error 404

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import Logo from '../../assets/logo.png'
 import './Menu.css';
 import Button from '../Button';
+import {Link} from 'react-router-dom';
 
 
 function Menu(){
   return(
     <nav className="Menu">
-      <a href="/">
+      <Link to="/">
         <img className="Logo" src={Logo} alt='logo'/>
-      </a>
+      </Link>
 
-      <Button as="a" className='ButtonLink' href="/">
+      <Button as={Link} className='ButtonLink' to="/cadastro/video">
         novo video
       </Button>
     </nav>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import styled from 'styled-components';
 
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Video';
 import CadastroCategoria from './pages/cadastro/Categoria';
+
+function pageError() {
+  return(
+    <Content>
+        <img src="https://fontmeme.com/permalink/200729/cd4070e711e18e89fd3aa40b540ca5a5.png" alt="error"/>
+    </Content>
+  );
+  
+}
+
+const Content = styled.div`
+  display: flex;
+  margin: 0 auto;
+  color: var(--white);
+  padding-top: 10%;
+`;
+
 
 ReactDOM.render(
   <BrowserRouter>
@@ -13,6 +31,7 @@ ReactDOM.render(
       <Route path='/' component={Home} exact />
       <Route path='/cadastro/video' component={CadastroVideo}/>
       <Route path='/cadastro/categoria' component={CadastroCategoria}/>
+      <Route component={pageError}/>
     </Switch>
   </BrowserRouter>,
   document.getElementById('root')
