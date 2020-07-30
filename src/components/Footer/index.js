9e4a01d20decdd8a3478d935c8cdaf838fb49621
yurifardel <yrnascimento@outlook.com>
2020-07-29 22:48:02 -0300

    using useState and making reused forms

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,8 +4,8 @@ import { FooterBase } from './styles';
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="https://github.com/yurifardel">
+        <img src="https://fontmeme.com/permalink/200729/3c3e823e0b4b6bb25c175a0e9c9329f2.png" alt="github" />
       </a>
       <p>
         Orgulhosamente criado durante a
